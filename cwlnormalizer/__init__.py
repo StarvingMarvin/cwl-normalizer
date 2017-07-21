8e5bdc15110370039ccd3e9546b00913fb347341
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2017-07-21 22:53:29 +0200

    couple of basic transforms

--- cwlnormalizer/__init__.py
+++ cwlnormalizer/__init__.py
@@ -4,7 +4,92 @@ import json
 __version__ = "0.1"
 
 
-def normalize(doc):
+def dict_to_list(obj, key='id', value=None):
+    if not isinstance(obj, dict):
+        return obj
+
+    ret = []
+    for k, v in obj.items():
+        if not isinstance(v, dict):
+            if value is None:
+                raise ValueError('dict values must be dicts')
+            else:
+                v = {value: v}
+        v[key] = k
+        ret.append(v)
+    return ret
+
+
+def wrap_to_list(obj):
+    if isinstance(obj, list):
+        return obj
+
+    return [obj]
+
+
+def expand_type(t):
+    if not isinstance(t, str):
+        return t
+
+    if t.endswith('?'):
+        t = [expand_type(t[:-1]), 'null']
+    elif t.endswith('[]'):
+        t = {
+            'type': 'array',
+            'items': expand_type(t[:-2])
+        }
+    return t
+
+
+def normalize_lists(doc):
+    for k in 'inputs', 'outputs':
+        if k in doc:
+            doc[k] = dict_to_list(doc[k], value='type')
+
+    if 'steps' in doc:
+        doc['steps'] = dict_to_list(doc['steps'])
+
+    for k in 'hints', 'requirements':
+        if k in doc:
+            doc[k] = dict_to_list(doc[k], key='class')
+
+    for step in doc.get('steps', []):
+        for k in 'in', 'out':
+            if k in step:
+                doc[k] = dict_to_list(doc[k])
+        for k in 'hints', 'requirements':
+            if k in step:
+                doc[k] = dict_to_list(doc[k], key='class')
+
+
+def normalize_arguments(doc):
+    if 'arguments' not in doc:
+        return
+
+    doc['arguments'] = [{'valueFrom': arg} if isinstance(arg, str) else arg for arg in doc['arguments']]
+
+
+def normalize_base_command(doc):
+    if 'baseCommand' not in doc:
+        return
+
+    doc['baseCommand'] = wrap_to_list(doc['baseCommand'])
+
+
+def normalize_steps(doc):
+    pass
+
+
+def normalize(doc, normalizers=None):
+    if normalizers is None:
+        normalizers = [
+            normalize_lists, normalize_arguments,
+            normalize_base_command, normalize_steps
+        ]
+
+    for n in normalizers:
+        n(doc)
+
     return doc
 
 
